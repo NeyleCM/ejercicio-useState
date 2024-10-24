7ac28190b5b89b09beee0678cf52189dd53873ae
NeyleCM <neylecmorantes@gmail.com>
2024-10-24 22:49:01 +0200

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
- //Aquí tu código
+const [name, setName] = useState('Sofía')
+const [newName, setNewName] = useState('')
+
+const hadleNameChange = (profesor) => {
+    setName(profesor)
+}
+
+const changeName = (e) => {
+    e.preventDefault()
+    if(newName.trim()) {
+        setName(newName)
+        setNewName('')
+        
+    }
+}
+
+return (
+    <>
+        <h2>Teacher name: {name}</h2>
+        <form onSubmit={changeName}>
+            <input 
+                type='text' 
+                value={newName}
+                onChange={(e) => setNewName(e.target.value)}
+                placeholder='Add a name'
+            />
+            <button type='submit'>Add</button>
+        </form>
+        <ul>
+            <li onClick={() => hadleNameChange('Data')}>Data</li>
+            <li onClick={() => hadleNameChange('Reyes')}>Reyes</li>
+            <li onClick={() => hadleNameChange('Soraya')}>Soraya</li>
+        </ul>
+    </>
+)
 }
 
 export default App
